bbc98db99bce80f659cf6ef734d7b601b19d30c8
Eric Bylaska <bylaska@gmail.com>
2022-01-01 22:56:12 -0800

    ...EJB

--- Tutorial/tutorial10.py
+++ Tutorial/tutorial10.py
@@ -97,7 +97,7 @@ def plot_pathenergy(plot,y0,machine,weights,xdata):
    y_imin    = 0.0
    ydiff_min = 99e99
 
-   delta = 0.1
+   #delta = 0.1
    ymin = +999999.9
    ymax = -999999.9
    y1 = []
@@ -121,9 +121,8 @@ def plot_pathenergy(plot,y0,machine,weights,xdata):
          y_imin    = y
          ydiff_min = ydiff
 
-
-   #delta = 0.1*(ymax-ymin)
    ### reset the plotting window and then plot data ###
+   delta = 0.1*(ymax-ymin)
    plot.resetwindow(0.0,ymin-delta,1.0,ymax+delta,"Scaled Energies")
    plot.plot1(y0,"blue")
    plot.plot1(y1,"red")
@@ -140,7 +139,8 @@ def plot_pathenergy(plot,y0,machine,weights,xdata):
 def plot_pathdiff(plot,y0,machine,weights,xdata):
    #colors = ("blue","green","yellow","purple","orange")
    #ncc    = len(colors)
-   delta = 0.1
+
+   #delta = 0.1
    ymin = +999999.9
    ymax = -999999.9
    y1 = []
@@ -161,191 +161,211 @@ def plot_pathdiff(plot,y0,machine,weights,xdata):
 
 ####################### main program ##############################
 
-### started from scratch - build up path from l=0 ###
-
-
-##### Read in fei file lazilly and do a simple print out of the data ####
-nframes = 0
-
-emin = +99999.99
-emax = -99999.99
-energies = []
-xinputs  = []
-ids      = []
-for (id,xdata,energy) in read_ml_urlfile(mlfilename):
-   print("nframes=",nframes)
-   xinputs.append(xdata)
-   energies.append(energy)
-   ids.append(id)
-   if (energy<emin): emin = energy
-   if (energy>emax): emax = energy
-
-   nframes += 1
-
-emid = (emax+emin)/2.0
-edif = (emax-emin)/2.0
-
-ninput   = len(xinputs[0])
-nframes0 = 0
-
-
-
-#beta = 2.0
-#sigmoid   = lambda x: 1.0/(1.0+math.exp(-x))
-#sigmoidp  = lambda x: math.exp(-x)/(1.0+math.exp(-x))**2
-#sigmoidpp = lambda x: math.exp(-x)*(math.exp(-x)-1.0)/(1.0+math.exp(-x))**3
-#ap = 1.0
-#xp = 4.5
-#bp = 3.0
-#penalty  = lambda x: ap*(0.5*(math.tanh(bp*(x-xp)) - math.tanh(bp*(x+xp))) + 1.0)
-#penaltyp = lambda x: ap*0.5*bp*( (1/math.cosh(bp*(x-xp)))**2 - (1.0/math.cosh(bp*(x+xp)))**2)
+def main():
 #
-#sigmoid   = lambda x: 0.5*(math.tanh(beta*x)+1.0)
-#sigmoidp  = lambda x: 0.5*beta*(1.0/math.cosh(beta*x))**2
-#sigmoidpp = lambda x: 0.5*(-2.0)*beta*beta*math.tanh(beta*x)*(1.0/math.sech(beta*x))**2
+
+   hidden_layers = [2,1]
+
+   ##### Read in fei file lazilly and do a simple print out of the data ####
+   nframes = 0
+   emin = +99999.99
+   emax = -99999.99
+   energies = []
+   xinputs  = []
+   ids      = []
+   for (id,xdata,energy) in read_ml_urlfile(mlfilename):
+      print("nframes=",nframes)
+      xinputs.append(xdata)
+      energies.append(energy)
+      ids.append(id)
+      if (energy<emin): emin = energy
+      if (energy>emax): emax = energy
+
+      nframes += 1
+
+   emid = (emax+emin)/2.0
+   edif = (emax-emin)/2.0
+
+   ninput   = len(xinputs[0])
+   nframes0 = 0
+
+
+   #beta = 2.0
+   #sigmoid   = lambda x: 1.0/(1.0+math.exp(-x))
+   #sigmoidp  = lambda x: math.exp(-x)/(1.0+math.exp(-x))**2
+   #sigmoidpp = lambda x: math.exp(-x)*(math.exp(-x)-1.0)/(1.0+math.exp(-x))**3
+   #ap = 1.0
+   #xp = 4.5
+   #bp = 3.0
+   #penalty  = lambda x: ap*(0.5*(math.tanh(bp*(x-xp)) - math.tanh(bp*(x+xp))) + 1.0)
+   #penaltyp = lambda x: ap*0.5*bp*( (1/math.cosh(bp*(x-xp)))**2 - (1.0/math.cosh(bp*(x+xp)))**2)
+   #
+   #sigmoid   = lambda x: 0.5*(math.tanh(beta*x)+1.0)
+   #sigmoidp  = lambda x: 0.5*beta*(1.0/math.cosh(beta*x))**2
+   #sigmoidpp = lambda x: 0.5*(-2.0)*beta*beta*math.tanh(beta*x)*(1.0/math.sech(beta*x))**2
  
-xmoid1    = lambda x: x
-xmoidp1   = lambda x: 1.0
-xmoidpp1  = lambda x: 0.0
+   xmoid1    = lambda x: x
+   xmoidp1   = lambda x: 1.0
+   xmoidpp1  = lambda x: 0.0
 
-relu      = lambda x: max(0.0,x)
-relup     = lambda x: 0.0 if (x<=0.0) else 1.0
-relupp    = lambda x: 0.0
+   relu      = lambda x: max(0.0,x)
+   relup     = lambda x: 0.0 if (x<=0.0) else 1.0
+   relupp    = lambda x: 0.0
 
-#bias = [[0.01],[0.01],[0.001],[0.0001],[0.00001],[0.0000001]]
-#bias = [[0.01],[0.01],[0.001]]
-#bias = []
+   #bias = [[0.01],[0.01],[0.001],[0.0001],[0.00001],[0.0000001]]
+   #bias = [[0.01],[0.01],[0.001]]
+   #bias = []
 
-machine = myfeedforward.MyFeedForward([ninput,2*ninput,2*ninput,1],[xmoid1,relu,relu,xmoid1],[xmoidp1,relup,relup,xmoidp1],[xmoidpp1,relupp,relupp,xmoidpp1])
-
-print("ninput=",ninput)
-print("emin=",emin)
-print("emax=",emax)
-print("emid=",emid)
-print("edif=",edif)
-
-### define the network topology ###
-#if os.path.isfile("tutorial10c.topology"):
-#   print("reading network topology")
-#   with open("tutorial10c.toplogy",'rb') as ff:
-#      network = pickle.loads(ff.read())
-#else:
-#   print("creating network topology")
-#   network = [[ninput,2*ninput,ninput,1],[xmoid1,relu,relu,xmoid1],[xmoidp1,relup,relup,xmoidp1],[xmoidpp1,relupp,relupp,xmoidpp1]]
-#   with open("tutorial10c.toplogy",'wb') as ff:
-#      ff.write(pickle.dumps(network))
-#network = [[ninput,2*ninput,2*ninput,1],[xmoid1,relu,relu,xmoid1],[xmoidp1,relup,relup,xmoidp1],[xmoidpp1,relupp,relupp,xmoidpp1]]
-
-print("creating network topology - n x (2n) x (2n) x 1")
-
-if os.path.isfile("tutorial10c.weights"):
-   print("reading weights")
-   with open("tutorial10c.weights",'rb') as ff:
-      weights = pickle.loads(ff.read())
-else:
-   print("creating initial weights")
-   weights = machine.initial_w()
-   for i in range(len(weights)):
-      weights[i] *= 1.0
+   ### define the network topology ###
+   #if os.path.isfile("tutorial10c.topology"):
+   #   print("reading network topology")
+   #   with open("tutorial10c.toplogy",'rb') as ff:
+   #      network = pickle.loads(ff.read())
+   #else:
+   #   print("creating network topology")
+   #   network = [[ninput,2*ninput,ninput,1],[xmoid1,relu,relu,xmoid1],[xmoidp1,relup,relup,xmoidp1],[xmoidpp1,relupp,relupp,xmoidpp1]]
+   #   with open("tutorial10c.toplogy",'wb') as ff:
+   #      ff.write(pickle.dumps(network))
 
 
-nw = len(weights)
-print("nw=",nw)
+   ### check/create network topology ###
+   print("creating network topology - n x hidden_layers x 1")
+   print("                          - hidden_layers = ( nlayers *",hidden_layers,")")
 
-print("nframes=",nframes)
+   ### define the network topology ###
+   ntwrk0 = [ninput]
+   ntwrkf = [xmoid1]
+   ntwrkp = [xmoidp1]
+   ntwrkpp= [xmoidpp1]
+   for h in range(len(hidden_layers)):
+      ntwrk0.append(hidden_layers[h]*ninput)
+      ntwrkf.append(relu)
+      ntwrkp.append(relup)
+      ntwrkpp.append(relupp)
+   ntwrk0.append(1)
+   ntwrkf.append(xmoid1)
+   ntwrkp.append(xmoidp1)
+   ntwrkpp.append(xmoidpp1)
 
-nepoch = 100
-nbatch = 1
-print("nepoch =",nepoch)
-print("nbatch =",nbatch)
+   #network = [[ninput,2*ninput,ninput,1],[xmoid1,relu,relu,xmoid1],[xmoidp1,relup,relup,xmoidp1],[xmoidpp1,relupp,relupp,xmoidpp1]]
+   #machine = myfeedforward.MyFeedForward(ninput,network[0],network[1],network[2],network[3])
+
+   machine = myfeedforward.MyFeedForward(ntwrk0,ntwrkf,ntwrkp,ntwrkpp)
+
+   print("ninput=",ninput)
+   print("emin=",emin)
+   print("emax=",emax)
+   print("emid=",emid)
+   print("edif=",edif)
 
 
-y = machine.evaluate(xinputs[0],weights)[0]
-print("y=",y)
+   if os.path.isfile("tutorial10c.weights"):
+      print("reading weights")
+      with open("tutorial10c.weights",'rb') as ff:
+         weights = pickle.loads(ff.read())
+   else:
+      print("creating initial weights")
+      weights = machine.initial_w()
+      for i in range(len(weights)):
+         weights[i] *= 1.0
 
-gg = machine.w_energy_gradient(xinputs[0],[energies[0]],weights)
-print("error=",gg[0]," len(gg[1])=",len(gg[1]))
+   nw = len(weights)
+   print("nw=",nw)
 
-###plot the relative energies using Turtle graphics###
-plot  = xyplotter.xyplotter(0.0,0.0,1.0,1.0, "Scaled Energies Plot",2)
-iydiff = plot_pathenergy(plot,energies[nframes0:],machine,weights,xinputs[nframes0:])
-id_min = ids[iydiff[0][0]]
-id_max = ids[iydiff[1][0]]
-print("iydiff=",iydiff," idmin=",id_min, " idmax=",id_max)
+   print("nframes=",nframes)
 
-#plot2 = xyplotter.xyplotter(0.0,0.0,1.0,1.0, "Scaled Diff Energies Plot",2)
-#yminmax = plot_pathdiff(plot2,energies[nframes0:],machine,weights,xinputs[nframes0:])
+   nepoch = 100
+   nbatch = 25
+   print("nepoch =",nepoch)
+   print("nbatch =",nbatch)
 
-enter0 = input(" -- start simulation --- ")
 
-alpha0 = 0.01
-alpha = 0.0001
-beta1 = 0.9
-beta2 = 0.999
-eps   = 1e-8
-
-beta1t = 1.0
-beta2t = 1.0
-m  = [0.0]*nw
-v  = [0.0]*nw
-g1 = [0.0]*nw
-error1 = 0.0
-
-for ep in range(nepoch):
-   ib      = 0
-   error1 = 0.0
-   for j in range(nw): m[j] = 0.0
-   for j in range(nw): v[j] = 0.0
-   for j in range(nw): g1[j] = 0.0
-
-   for i in range(nframes):
-      ib  += 1
-      gg = machine.w_energy_gradient(xinputs[i],[energies[i]],weights)
-      error1 += gg[0]
-      for j in range(nw): g1[j] += gg[1][j]
-
-      if ((ib>=nbatch) or (i>=(nframes-1))):
-         xx = 1.0/float(ib)
-         error1 *= xx
-         for j in range(nw): g1[j] *= xx
-
-         beta1t *= beta1
-         beta2t *= beta2
-         if (beta1t<1.0e-20): beta1t = 0.0
-         if (beta2t<1.0e-20): beta2t = 0.0
-         alphat = alpha*math.sqrt(1.0-beta2t)/(1.0-beta1t)
-         for j in range(nw):
-            m[j] = beta1*m[j] + (1.0-beta1)*g1[j]
-            v[j] = beta2*v[j] + (1.0-beta2)*g1[j]*g1[j]
-            weights[j] -= alphat*m[j]/(math.sqrt(v[j]) + eps)
-         ib = 0
-
-   print("alphat=",alphat, " beta1t=",beta1t," beta2t=",beta2t," eps=",eps)
-   print("    beta1=",beta1," beta2=",beta2," error1=", error1)
-
-   ### plot the relative energies using turtle graphicsl ###
+   ###plot the relative energies using Turtle graphics###
+   plot  = xyplotter.xyplotter(0.0,0.0,1.0,1.0, "Scaled Energies Plot",2)
    iydiff = plot_pathenergy(plot,energies[nframes0:],machine,weights,xinputs[nframes0:])
    id_min = ids[iydiff[0][0]]
    id_max = ids[iydiff[1][0]]
+   print("iydiff=",iydiff," idmin=",id_min, " idmax=",id_max)
+   plot2 = xyplotter.xyplotter(0.0,0.0,1.0,1.0, "Scaled Diff Energies Plot",2)
+   yminmax = plot_pathdiff(plot2,energies[nframes0:],machine,weights,xinputs[nframes0:])
 
-   #yminmax = plot_pathdiff(plot2,energies[nframes0:],machine,weights,xinputs[nframes0:])
+   enter0 = input(" -- start simulation --- ")
 
-   msg  = "epoch=%5d " % (ep+1)
-   msg += "| min=%5d %6.3f %6.3f %8.3e %5s " % (iydiff[0][0],iydiff[0][1],iydiff[0][2],iydiff[0][3],id_min)
-   msg += "| max=%5d %6.3f %6.3f %8.3e %5s " % (iydiff[1][0],iydiff[1][1],iydiff[1][2],iydiff[1][3],id_max)
-   msg += "|| error = %12.8e" % (math.sqrt(error1))
-   print(msg)
+   alpha = 0.0001
+   beta1 = 0.9
+   beta2 = 0.999
+   eps   = 1e-8
+   beta1t = 1.0
+   beta2t = 1.0
+   m    = [0.0]*nw
+   v    = [0.0]*nw
+   gsum = [0.0]*nw
+
+   for epoch in range(nepoch):
+      batch   = 0
+      error1 = 0.0
+      for j in range(nw): gsum[j] = 0.0
+   
+      for i in range(nframes):
+         batch += 1
+         gg = machine.w_energy_gradient(xinputs[i],[energies[i]],weights)
+         error = gg[0]
+         g1    = gg[1]
+
+         error1 += error
+         for j in range(nw): 
+            gsum[j] += g1[j]
+
+         if ((batch>=nbatch) or (i>=(nframes-1))):
+            xx = 1.0/float(batch)
+            for j in range(nw): 
+               gsum[j] *= xx
+
+            beta1t *= beta1
+            beta2t *= beta2
+            if (beta1t<1.0e-20): beta1t = 0.0
+            if (beta2t<1.0e-20): beta2t = 0.0
+            alphat = alpha*math.sqrt(1.0-beta2t)/(1.0-beta1t)
+
+            for j in range(nw):
+               m[j] = beta1*m[j] + (1.0-beta1)*gsum[j]
+               v[j] = beta2*v[j] + (1.0-beta2)*(gsum[j]*gsum[j])
+               weights[j] -= alphat*m[j]/(math.sqrt(v[j]) + eps)
+
+            batch = 0
+            for j in range(nw): gsum[j] = 0.0
+
+      ### plot the relative energies using turtle graphicsl ###
+      iydiff = plot_pathenergy(plot,energies[nframes0:],machine,weights,xinputs[nframes0:])
+      id_min = ids[iydiff[0][0]]
+      id_max = ids[iydiff[1][0]]
+      yminmax = plot_pathdiff(plot2,energies[nframes0:],machine,weights,xinputs[nframes0:])
+
+      ### print out update ###
+      msg  = "epoch=%5d nframes=%5d nbatch=%3d " % (epoch+1,nframes,nbatch)
+      msg += "| min=%5d %6.3f %6.3f %8.3e %5s " % (iydiff[0][0],iydiff[0][1],iydiff[0][2],iydiff[0][3],id_min)
+      msg += "| max=%5d %6.3f %6.3f %8.3e %5s " % (iydiff[1][0],iydiff[1][1],iydiff[1][2],iydiff[1][3],id_max)
+      msg += "|| error = %12.8e" % (math.sqrt(error1))
+      print(msg)
+
+      ### writeout weights ###
+      with open("tutorial10c.weights",'wb') as ff:
+         ff.write(pickle.dumps(weights))
+
+   ### end for epoch ###
+
+
+   ### wait for return so that plot can be seen ###
+   x = input("--Press return to finish--")
 
    ### writeout weights ###
    with open("tutorial10c.weights",'wb') as ff:
       ff.write(pickle.dumps(weights))
 
+   return
 
-### wait for return so that plot can be seen ###
-x = input("--Press return to finish--")
 
-### writeout weights ###
-with open("tutorial10c.weights",'wb') as ff:
-   ff.write(pickle.dumps(weights))
+if __name__ == "__main__":
+   main()
 
 
